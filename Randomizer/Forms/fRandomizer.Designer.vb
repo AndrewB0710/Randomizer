881a273672b329dd3dd5df68a34cc766404316cd
AndrewB0710 <andrewbauer1001@gmail.com>
2021-11-18 02:13:48 -0500

    Added features

--- Randomizer/Forms/fRandomizer.Designer.vb
+++ Randomizer/Forms/fRandomizer.Designer.vb
@@ -24,6 +24,7 @@ Partial Class fRandomizer
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(fRandomizer))
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btnSettings = New System.Windows.Forms.Button()
         Me.lblRandomName = New System.Windows.Forms.Label()
         Me.btnRandomize = New System.Windows.Forms.Button()
         Me.Panel3 = New System.Windows.Forms.Panel()
@@ -36,6 +37,8 @@ Partial Class fRandomizer
         Me.btnDelete = New System.Windows.Forms.Button()
         Me.lstNames = New System.Windows.Forms.ListBox()
         Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.btnDeleteAll = New System.Windows.Forms.Button()
+        Me.btnShuffle = New System.Windows.Forms.Button()
         Me.Panel1.SuspendLayout()
         Me.Panel3.SuspendLayout()
         CType(Me.nmQuantity, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -45,31 +48,42 @@ Partial Class fRandomizer
         'Panel1
         '
         Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.btnSettings)
         Me.Panel1.Controls.Add(Me.lblRandomName)
         Me.Panel1.Controls.Add(Me.btnRandomize)
         Me.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom
         Me.Panel1.Location = New System.Drawing.Point(0, 484)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(234, 91)
+        Me.Panel1.Size = New System.Drawing.Size(273, 91)
         Me.Panel1.TabIndex = 7
+        '
+        'btnSettings
+        '
+        Me.btnSettings.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.btnSettings.Location = New System.Drawing.Point(205, 63)
+        Me.btnSettings.Name = "btnSettings"
+        Me.btnSettings.Size = New System.Drawing.Size(63, 23)
+        Me.btnSettings.TabIndex = 2
+        Me.btnSettings.Text = "Settings"
+        Me.btnSettings.UseVisualStyleBackColor = True
         '
         'lblRandomName
         '
         Me.lblRandomName.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.lblRandomName.Location = New System.Drawing.Point(23, 11)
+        Me.lblRandomName.Location = New System.Drawing.Point(12, 11)
         Me.lblRandomName.Name = "lblRandomName"
-        Me.lblRandomName.Size = New System.Drawing.Size(181, 40)
+        Me.lblRandomName.Size = New System.Drawing.Size(248, 40)
         Me.lblRandomName.TabIndex = 1
         Me.lblRandomName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'btnRandomize
         '
         Me.btnRandomize.Anchor = System.Windows.Forms.AnchorStyles.Bottom
-        Me.btnRandomize.Location = New System.Drawing.Point(59, 63)
+        Me.btnRandomize.Location = New System.Drawing.Point(81, 63)
         Me.btnRandomize.Name = "btnRandomize"
         Me.btnRandomize.Size = New System.Drawing.Size(109, 23)
         Me.btnRandomize.TabIndex = 0
-        Me.btnRandomize.Text = "Randomize"
+        Me.btnRandomize.Text = "Random Select"
         Me.btnRandomize.UseVisualStyleBackColor = True
         '
         'Panel3
@@ -84,14 +98,14 @@ Partial Class fRandomizer
         Me.Panel3.Dock = System.Windows.Forms.DockStyle.Bottom
         Me.Panel3.Location = New System.Drawing.Point(0, 377)
         Me.Panel3.Name = "Panel3"
-        Me.Panel3.Size = New System.Drawing.Size(234, 107)
+        Me.Panel3.Size = New System.Drawing.Size(273, 107)
         Me.Panel3.TabIndex = 0
         '
         'lblQuantity
         '
-        Me.lblQuantity.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.lblQuantity.Anchor = System.Windows.Forms.AnchorStyles.Bottom
         Me.lblQuantity.AutoSize = True
-        Me.lblQuantity.Location = New System.Drawing.Point(26, 59)
+        Me.lblQuantity.Location = New System.Drawing.Point(56, 59)
         Me.lblQuantity.Name = "lblQuantity"
         Me.lblQuantity.Size = New System.Drawing.Size(53, 15)
         Me.lblQuantity.TabIndex = 6
@@ -108,8 +122,8 @@ Partial Class fRandomizer
         '
         'nmQuantity
         '
-        Me.nmQuantity.Anchor = System.Windows.Forms.AnchorStyles.Right
-        Me.nmQuantity.Location = New System.Drawing.Point(26, 77)
+        Me.nmQuantity.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.nmQuantity.Location = New System.Drawing.Point(56, 77)
         Me.nmQuantity.Name = "nmQuantity"
         Me.nmQuantity.Size = New System.Drawing.Size(47, 23)
         Me.nmQuantity.TabIndex = 4
@@ -117,8 +131,8 @@ Partial Class fRandomizer
         '
         'btnAdd
         '
-        Me.btnAdd.Anchor = System.Windows.Forms.AnchorStyles.Right
-        Me.btnAdd.Location = New System.Drawing.Point(95, 77)
+        Me.btnAdd.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.btnAdd.Location = New System.Drawing.Point(109, 77)
         Me.btnAdd.Name = "btnAdd"
         Me.btnAdd.Size = New System.Drawing.Size(109, 23)
         Me.btnAdd.TabIndex = 2
@@ -130,7 +144,7 @@ Partial Class fRandomizer
         Me.txtNames.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.txtNames.Location = New System.Drawing.Point(11, 25)
         Me.txtNames.Name = "txtNames"
-        Me.txtNames.Size = New System.Drawing.Size(210, 23)
+        Me.txtNames.Size = New System.Drawing.Size(249, 23)
         Me.txtNames.TabIndex = 1
         '
         'Panel4
@@ -139,15 +153,15 @@ Partial Class fRandomizer
         Me.Panel4.Dock = System.Windows.Forms.DockStyle.Top
         Me.Panel4.Location = New System.Drawing.Point(0, 0)
         Me.Panel4.Name = "Panel4"
-        Me.Panel4.Size = New System.Drawing.Size(234, 0)
+        Me.Panel4.Size = New System.Drawing.Size(273, 0)
         Me.Panel4.TabIndex = 1
         '
         'btnDelete
         '
         Me.btnDelete.Anchor = System.Windows.Forms.AnchorStyles.Bottom
-        Me.btnDelete.Location = New System.Drawing.Point(60, 348)
+        Me.btnDelete.Location = New System.Drawing.Point(110, 348)
         Me.btnDelete.Name = "btnDelete"
-        Me.btnDelete.Size = New System.Drawing.Size(109, 23)
+        Me.btnDelete.Size = New System.Drawing.Size(53, 23)
         Me.btnDelete.TabIndex = 3
         Me.btnDelete.Text = "Delete"
         Me.btnDelete.UseVisualStyleBackColor = True
@@ -161,11 +175,13 @@ Partial Class fRandomizer
         Me.lstNames.ItemHeight = 15
         Me.lstNames.Location = New System.Drawing.Point(12, 8)
         Me.lstNames.Name = "lstNames"
-        Me.lstNames.Size = New System.Drawing.Size(210, 334)
+        Me.lstNames.Size = New System.Drawing.Size(249, 334)
         Me.lstNames.TabIndex = 0
         '
         'Panel2
         '
+        Me.Panel2.Controls.Add(Me.btnDeleteAll)
+        Me.Panel2.Controls.Add(Me.btnShuffle)
         Me.Panel2.Controls.Add(Me.lstNames)
         Me.Panel2.Controls.Add(Me.btnDelete)
         Me.Panel2.Controls.Add(Me.Panel4)
@@ -173,18 +189,40 @@ Partial Class fRandomizer
         Me.Panel2.Dock = System.Windows.Forms.DockStyle.Fill
         Me.Panel2.Location = New System.Drawing.Point(0, 0)
         Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(234, 484)
+        Me.Panel2.Size = New System.Drawing.Size(273, 484)
         Me.Panel2.TabIndex = 8
+        '
+        'btnDeleteAll
+        '
+        Me.btnDeleteAll.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.btnDeleteAll.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.btnDeleteAll.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btnDeleteAll.Location = New System.Drawing.Point(169, 348)
+        Me.btnDeleteAll.Name = "btnDeleteAll"
+        Me.btnDeleteAll.Size = New System.Drawing.Size(72, 23)
+        Me.btnDeleteAll.TabIndex = 5
+        Me.btnDeleteAll.Text = "Delete All"
+        Me.btnDeleteAll.UseVisualStyleBackColor = False
+        '
+        'btnShuffle
+        '
+        Me.btnShuffle.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.btnShuffle.Location = New System.Drawing.Point(32, 348)
+        Me.btnShuffle.Name = "btnShuffle"
+        Me.btnShuffle.Size = New System.Drawing.Size(72, 23)
+        Me.btnShuffle.TabIndex = 4
+        Me.btnShuffle.Text = "Shuffle"
+        Me.btnShuffle.UseVisualStyleBackColor = True
         '
         'fRandomizer
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(234, 575)
+        Me.ClientSize = New System.Drawing.Size(273, 575)
         Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Panel1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.MinimumSize = New System.Drawing.Size(250, 39)
+        Me.MinimumSize = New System.Drawing.Size(260, 39)
         Me.Name = "fRandomizer"
         Me.Text = "Randomizer"
         Me.Panel1.ResumeLayout(False)
@@ -209,4 +247,7 @@ Partial Class fRandomizer
     Friend WithEvents btnDelete As Button
     Friend WithEvents lstNames As ListBox
     Friend WithEvents Panel2 As Panel
+    Friend WithEvents btnDeleteAll As Button
+    Friend WithEvents btnShuffle As Button
+    Friend WithEvents btnSettings As Button
 End Class
---
+++ Randomizer/Forms/fRandomizer.vb
@@ -0,0 +1,96 @@
+﻿Public Class fRandomizer
+
+    Private deleteAllOfPickedItem As Boolean = True
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+        Dim Count As Integer
+        Dim i As Integer
+
+        Count = nmQuantity.Value
+
+        If txtNames.Text.Trim.Length = 0 Then
+            MsgBox("Entry can not contain only whitespace OR be a quantity of 0", 0, "Error")
+        Else
+            While i <> Count
+                lstNames.Items.Add(txtNames.Text)
+                i = i + 1
+            End While
+        End If
+
+        txtNames.Clear()
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+
+        lstNames.Items.Remove(lstNames.SelectedItem)
+    End Sub
+
+    Private Sub btnRandomize_Click(sender As Object, e As EventArgs) Handles btnRandomize.Click
+        Dim myRandom As New Random
+        Dim i As Integer = lstNames.Items.Count - 1
+
+        lblRandomName.Text = lstNames.Items.Item(myRandom.Next(i)).ToString
+
+        If deleteAllOfPickedItem = False Then
+            lstNames.Items.Remove(lblRandomName.Text)
+        ElseIf deleteAllOfPickedItem = True Then
+            For index = lstNames.Items.Count - 1 To 0 Step -1
+                If lstNames.Items(index) = lblRandomName.Text Then
+                    lstNames.Items.RemoveAt(index)
+                End If
+            Next
+        End If
+    End Sub
+
+    Private Sub fRandomizer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblRandomName.Font = New Font("Times New Roman", 18)
+
+        Me.AcceptButton = btnAdd
+        btnAdd.DialogResult = System.Windows.Forms.DialogResult.OK
+    End Sub
+
+    Private Sub btnShuffle_Click(sender As Object, e As EventArgs) Handles btnShuffle.Click
+        Dim random As New Random()
+
+        For count = 0 To lstNames.Items.Count - 1
+            Dim index As Integer = random.Next(count, lstNames.Items.Count)
+            If count <> index Then
+                Dim temp = lstNames.Items(count)
+                lstNames.Items(count) = lstNames.Items(index)
+                lstNames.Items(index) = temp
+            End If
+        Next
+    End Sub
+
+    Private Sub btnDeleteAll_Click(sender As Object, e As EventArgs) Handles btnDeleteAll.Click
+        Dim Response = MsgBox("Are you sure you want to delete the entire list?", 4, "Warning")
+        If Response = vbNo Then
+            'Do Nothing
+        ElseIf Response = vbYes Then
+            lstNames.Items.Clear()
+        End If
+    End Sub
+
+    Private Sub btnSettings_Click(sender As Object, e As EventArgs) Handles btnSettings.Click
+
+        If deleteAllOfPickedItem = True Then
+            Dim Response = MsgBox("Current random selects delete all entrys with a similar name. Would you like to change this to delete only one entry?",
+                              4, "Change Selection Removal")
+            If Response = vbNo Then
+                'Do nothing
+            ElseIf Response = vbYes Then
+                deleteAllOfPickedItem = False
+            End If
+        ElseIf deleteAllOfPickedItem = False Then
+            Dim Response = MsgBox("Current random selects delete one entry. Would you like to change this to delete all entries with a similar name?",
+                              4, "Change Selection Removal")
+            If Response = vbNo Then
+                'Do nothing
+            ElseIf Response = vbYes Then
+                deleteAllOfPickedItem = True
+            End If
+        End If
+
+    End Sub
+End Class
